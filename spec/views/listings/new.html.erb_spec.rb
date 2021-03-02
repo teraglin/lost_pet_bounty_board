a0d1704ac42ea785d477fe64ecefdf08ebe84fb9
teraglin <apkirvan@hotmail.com>
2021-03-02 15:23:05 +1100

    added Listings via acaffold

---
+++ spec/views/listings/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "listings/new", type: :view do
+  before(:each) do
+    assign(:listing, Listing.new(
+      title: "MyString",
+      description: "MyText",
+      bounty: 1,
+      completed: false
+    ))
+  end
+
+  it "renders new listing form" do
+    render
+
+    assert_select "form[action=?][method=?]", listings_path, "post" do
+
+      assert_select "input[name=?]", "listing[title]"
+
+      assert_select "textarea[name=?]", "listing[description]"
+
+      assert_select "input[name=?]", "listing[bounty]"
+
+      assert_select "input[name=?]", "listing[completed]"
+    end
+  end
+end
