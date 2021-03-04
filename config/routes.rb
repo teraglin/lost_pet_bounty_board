5031f4d571e7ccb2a383218f500396fdeafa38e7
teraglin <apkirvan@hotmail.com>
2021-03-04 15:45:19 +1100

    added navbar with home, registration and log in/out features. registration link dissappears when loged in.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
 
-  root "listings#index"
+  root 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
