9a9c4809145d0dd9bffdac2fdfa654059dc62d58
teraglin <apkirvan@hotmail.com>
2021-03-15 15:42:31 +1100

    tailwind reading scss errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "stylesheets/application"
+import "../stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
