fe1fae0c7d02c642c32cb6e45002d76db8a4ca50
teraglin <apkirvan@hotmail.com>
2021-03-19 17:52:35 +1100

    added validation for datetime and sanitisation in listings/index.html.erb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,9 +10,21 @@ class Listing < ApplicationRecord
     validates_presence_of :lost_date
     validates_presence_of :image
 
+    validate :is_valid_datetime
+
     private
     
     def remove_whitespace
         self.title = self.title.strip
     end
+      
+    def is_valid_datetime
+      if self.lost_date > Time.now
+        errors.add(:lost_date, "must be a past date")
+      elsif self.lost_date.strftime("%Y").to_i < 2000
+        errors.add(:lost_date, "cannot be earlier than the year 2000")
+      end
+    #   self.lost_date.strftime("%Y").to_i < 2000 || self.lost_date.strftime("%Y").to_i > Time.year.now
+    end
+      
 end
